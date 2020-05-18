ac61bf42b42c8042abb225efd44b70092aaccf18
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-05-17 22:27:51 -0700

    saving before I mess with webpack

--- client/src/components/Specials.jsx
+++ client/src/components/Specials.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FeaturesAd from './images/specials/feat.png';
+import Weekly from './images/specials/weekly.png';
 
 
  const Specials = (props) => {
 
   return (
+<div>
 
     <div className="specialsTextLeft headline main-typeface"> Weekly Ad
-    <hr/>
+
     </div>
 
-    <img className="specials-img-left img-specs" src="/images/specials/weekly.png" />
+      <div>
+        <img src={Weekly} alt='weekly ad'/>
+      </div>
+</div>
 
-    <div className="specDetails typeface">
-      <a onclick="specialsModalViewer('left')">see all items &#10095;&#10095;</a>
-    </div>
 
-    <div className="specialsTextRight headline main-typeface"> Featured Items
-      <hr/>
-    </div>
-    // <img className="specialsImgRight imgSpecs" src="/images/specials/feat.png" />
-    <div className="specDetails typeface">
-      <a onclick="specialsModalViewer('right')">see all items &#10095;&#10095;</a>
-    </div>
-
-  )
+)
 }
-
 export default Specials
+
