765cb733f6eae714e509f0d7b5879660047b57ed
Bryan Evangelista <bryanevangelista@Bryans-MBP.socal.rr.com>
2020-05-18 15:16:58 -0700

    completed header;

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,20 +5,18 @@ export default function Header(props){
   return (
   <header>
       <img className="image" src={Logo} alt='weekly ad' />
-      <div className="text typeface">
-        <div>750 S. Alameda Street</div>
-        <div>Los Angeles, CA 90021</div>
-        <div><strong>Tel:</strong> (213) 688-7707</div>
-        <div><strong>Fax:</strong> (213) 688-1121</div>
-        <div>
+      <ul id="location-text" className="text typeface">
+        <li>750 S. Alameda Street</li>
+        <li>Los Angeles, CA 90021</li>
+        <li><strong>Tel:</strong> (213) 688-7707</li>
+        <li><strong>Fax:</strong> (213) 688-1121</li>
+        <li>
           <strong>Email: &nbsp; </strong>
           <a href="mailto:Corporate@cantonfoodcocom">
             Corporate@cantonfoodco.com
             </a>
-        </div>
-      </div>
+        </li>
+      </ul>
     </header>
   )
 }
-
-
